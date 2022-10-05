a56aed11c203e0d0d788c7de3783e8246b1cb660
Aaron <aaron.carton@student.howest.be>
2022-10-05 22:39:00 +0200

    add Vetur config to pwa

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+export default {}
 </script>
 
 <template>
-<p class="text-orange-500 text-4xl font-bold">It's pizza time</p>
+  <p class="text-orange-500 text-4xl font-bold">It's pizza time</p>
 </template>
-
---
+++ vetur.config.js
@@ -0,0 +1,10 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  projects: [
+    {
+      root: './pwa',
+      globalComponents: ['./src/components/**/*.vue'],
+    },
+  ],
+}
