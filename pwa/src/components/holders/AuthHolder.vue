13a901b30a0be63194c7604a50c8a7c4710103c9
DefoortMerijn <merijn.defoort@student.howest.be>
2022-10-16 15:44:19 +0200

    Feat: Setup Frontend + Firebase

---
+++ pwa/src/components/holders/AuthHolder.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div class="grid min-h-screen place-items-center bg-neutral-50">
+      <div class="w-full max-w-lg px-6">
+        <h1 class="mb-6 text-4xl font-bold text-neutral-800">PizzatTime</h1>
+  
+        <div class="mb-20 rounded-md bg-white p-6 shadow">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </template>
