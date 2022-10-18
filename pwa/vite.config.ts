b232bca640c6d297adcf31f22d30f7c05da87565
DefoortMerijn <merijn.defoort@student.howest.be>
2022-10-19 00:48:48 +0200

    Feat: Home + small route edits

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue(),
+    Unocss({
+      rules: [['font-title', { 'font-family': 'barber-complete, sans-serif;' }]],
+    }),
+    VitePluginFonts({
+      typekit: {
+        id: 'cst1irc',
+        defer: true,
+        injectTo: 'head',
+      },
+    }),
+  ],
 })
