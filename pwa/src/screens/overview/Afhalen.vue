8f11e6b8fbc249c836695fab07a160d3969f29db
Aaron <aaron.carton@student.howest.be>
2022-10-29 15:42:28 +0200

    feat: convert Cart to component

--- pwa/src/screens/overview/Afhalen.vue
+++ pwa/src/screens/overview/Afhalen.vue
@@ -45,27 +45,7 @@
             </div>
           </div>
         </div>
-        <div
-          class="row-end-8 col-span-1 col-start-4 row-span-1 row-start-1 hidden flex-col rounded-lg bg-neutral-50 p-3 sm:flex"
-        >
-          <h2 class="text-2xl font-semibold">Order</h2>
-          <div class="flex w-full items-center justify-between p-4">
-            <p class="text-xl">1x</p>
-            <h2 class="justify-self-start text-xl font-bold">Margherita</h2>
-            <p class="self-end justify-self-end text-xl">€16.99</p>
-          </div>
-          <div
-            class="mt-110 mb-3 flex w-full justify-between rounded-lg bg-neutral-200 p-3"
-          >
-            <p class="text-xl font-bold">Total price</p>
-            <p class="text-xl">€16.99</p>
-          </div>
-          <button
-            class="w-full rounded-lg bg-red-700 px-6 py-2 font-bold text-neutral-50"
-          >
-            Checkout
-          </button>
-        </div>
+        <Cart />
       </div>
     </div>
   </div>
@@ -74,11 +54,16 @@
 <script lang="ts">
 import { computed } from 'vue'
 import { useQuery } from '@vue/apollo-composable'
-import OverviewNav from '../../components/generic/OverviewNav.vue'
-import Pizza from '../../interfaces/pizza.interface'
 import { PIZZAS } from '../../graphql/query.pizza'
+import Cart from '../../components/generic/Cart.vue'
+import Pizza from '../../interfaces/pizza.interface'
+import OverviewNav from '../../components/generic/OverviewNav.vue'
 
 export default {
+  components: {
+    Cart,
+    OverviewNav,
+  },
   setup() {
     const { result, loading, error } = useQuery(PIZZAS)
     const orders: any[] = []
@@ -100,10 +85,6 @@ export default {
       orders,
       addToCart,
     }
-  },
-
-  components: {
-    OverviewNav,
   },
 }
 </script>
