b0e25d5b83eb233f722a08d031eb9822b3bf38f6
DefoortMerijn <merijn.defoort@student.howest.be>
2022-11-01 15:59:06 +0100

    Feat: Customize start + little route fixes

--- pwa/src/screens/overview/OverviewHolder.vue
+++ pwa/src/screens/overview/OverviewHolder.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-[url(/HomeBG.jpg)] sm:bg-cover">
-    <div class="bg-opacity-65 min-h-screen bg-black py-6 px-12 sm:px-60">
-      <div class="grid-rows-auto grid grid-cols-4 gap-6">
+  <div class="h-fulln overflow-y-hidden bg-[url(/HomeBG.jpg)] sm:bg-auto">
+    <div class="bg-opacity-65 max-h-screen bg-black py-6 px-12 sm:px-60">
+      <div class="grid-rows-12 grid max-h-screen grid-cols-4 gap-6">
         <router-view />
         <Cart />
       </div>
