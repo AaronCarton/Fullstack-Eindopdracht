bfb98089a9c6dc3a94267f1be3cbb818593bc388
DefoortMerijn <merijn.defoort@student.howest.be>
2022-11-30 00:59:04 +0100

    Feat: Restyled Payment screen

--- pwa/src/screens/payment/index.vue
+++ pwa/src/screens/payment/index.vue
@@ -1,78 +1,166 @@
 <template>
-  <div class="grid-rows-auto grid w-full gap-1 overflow-auto rounded-lg bg-white px-3 py-3">
-    <!--Payment methodes-->
-    <div class="flex flex-col gap-1">
-      <label class="font-semibold lg:text-xl" for="time">Select a desired moment</label>
-      <select
-        class="max-w-auto border-1 h-auto gap-2 rounded-lg border-neutral-400 bg-white py-1 px-2 font-semibold lg:text-xl"
-        value="As fast as possible"
-        id="time"
-        v-if="times.length > 0"
-      >
-        <option value="As fast as possible" selected>As fast as possible</option>
-        <option v-for="time in times" :key="time" :value="time">
-          {{ time }}
-        </option>
-      </select>
-      <select v-else name="As fast as possible" placeholder="As fast as possible">
-        <option value="As fast as possible" selected>As fast as possible</option>
-        <option class="bg-neutral-200 text-center text-neutral-500" value="No options" disabled>
-          No options
-        </option>
-      </select>
-    </div>
-    <div class="py-3">
-      <h2 class="text-lg font-bold lg:text-2xl">Payment methodes</h2>
-
-      <div class="mt-5 flex items-center">
-        <PaymentForm :methode="m" />
-      </div>
-    </div>
-
-    <!--information for delivery-->
-    <form class="row-span-3">
-      <h2 class="mb-3 text-lg font-bold lg:text-2xl">{{ deliveryType }} information</h2>
-      <div class="grid grid-cols-1">
-        <div class="col-span-1 grid rounded-lg p-6">
+  <div class="flex w-full flex-col justify-between gap-1 overflow-auto rounded-lg bg-white p-6">
+    <div>
+      <h1 class="mb-6 text-center text-4xl font-bold">Checkout</h1>
+      <!--Payment methodes-->
+      <div class="grid grid-cols-2 gap-5">
+        <div>
           <div class="flex flex-col gap-1">
-            <label class="font-semibold lg:text-xl" for="">Delivery date</label>
-            <input
-              type="date"
-              class="w-50 h-auto rounded-lg border-2 border-neutral-200 bg-white px-2 py-1 font-semibold lg:text-xl"
-            />
+            <h2 class="mb-3 text-lg font-semibold lg:text-2xl">Select a desired moment</h2>
+            <select
+              class="rounded-lg border-2 border-neutral-200 bg-white p-2"
+              value="As fast as possible"
+              id="time"
+              v-if="times.length > 0"
+            >
+              <option value="As fast as possible" selected>As fast as possible</option>
+              <option v-for="time in times" :key="time" :value="time">
+                {{ time }}
+              </option>
+            </select>
+            <select
+              v-else
+              name="As fast as possible"
+              placeholder="As fast as possible"
+              class="max-w-auto border-1 h-auto gap-2 rounded-lg border-neutral-400 bg-white p-3"
+            >
+              <option value="As fast as possible" selected hidden>As fast as possible</option>
+              <option
+                class="bg-neutral-200 text-center text-neutral-500"
+                value="No options"
+                disabled
+              >
+                No options
+              </option>
+            </select>
+          </div>
+          <div class="py-3">
+            <h2 class="mb-3 text-lg font-semibold capitalize lg:text-2xl">
+              {{ deliveryType }} address
+            </h2>
+            <div v-if="deliveryType === 'takeaway'">
+              <div class="border-1 rounded-md border-neutral-400 bg-neutral-50 p-2">
+                <p class="font-semibold">Restaurant Kortrijk</p>
+                <p class="text-neutral-400">President Kennedylaan 100, 8500 Kortrijk</p>
+              </div>
+            </div>
+            <div v-else>
+              <div class="gap-3 rounded-lg">
+                <div class="flex flex-col gap-1">
+                  <label class="font-medium lg:text-lg" for="">Street name</label>
+                  <input
+                    type="text"
+                    class="h-auto rounded-lg border-2 border-neutral-200 bg-white p-2"
+                    placeholder="ex. Nowherestreet"
+                    :value="user?.street"
+                  />
+                </div>
+                <div class="col-span-1 flex flex-col gap-1">
+                  <label class="font-medium lg:text-lg" for="">Street nr.</label>
+                  <input
+                    type="number"
+                    class="h-auto rounded-lg border-2 border-neutral-200 bg-white p-2"
+                    placeholder="ex. 155"
+                    maxlength="4"
+                    :value="user?.houseNumber"
+                  />
+                </div>
+                <div class="col-span-1 flex flex-col gap-1">
+                  <label class="font-medium lg:text-lg" for="pC">Postal code</label>
+                  <div
+                    class="flex justify-between rounded-lg border-2 border-neutral-200 p-2 peer-focus:border-red-400"
+                  >
+                    <input
+                      type="text"
+                      class="w-full bg-white focus:outline-none"
+                      placeholder="ex. Kortrijk or 8500"
+                      id="pC"
+                      v-model="city"
+                    />
+                    <Clear @click="emptyInput()" class="cursor-pointer" />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div
-          v-if="!user?.street && !user?.city && !user?.houseNumber"
-          class="col-span-1 grid grid-cols-3 gap-3 rounded-lg p-6"
-        >
-          <div class="col-span-2 flex flex-col gap-1">
-            <label class="font-bold lg:text-xl" for="">Street name</label>
-            <input
-              type="text"
-              class="h-auto rounded-lg border-2 border-neutral-200 bg-white py-1 px-2 text-xl font-semibold"
-            />
-          </div>
-          <div class="col-span-1 flex flex-col gap-1">
-            <label class="text-xl font-bold" for="">Street nr.</label>
-            <input
-              type="number"
-              class="h-auto rounded-lg border-2 border-neutral-200 bg-white py-1 px-2 text-xl font-semibold"
-            />
-          </div>
-          <div class="col-span-1 flex flex-col gap-1">
-            <label class="text-xl font-bold" for="">Postal code</label>
-            <input
-              type="text"
-              class="h-auto rounded-lg border-2 border-neutral-200 bg-white py-1 px-2 text-xl font-semibold"
-            />
-          </div>
+        <div>
+          <h2 class="mb-3 text-lg font-semibold capitalize lg:text-2xl">Payment Methodes</h2>
+          <!-- <PaymentForm :methode="m" /> -->
+          <RadioGroup v-model="selected">
+            <RadioGroupOption class="" value="Online" v-slot="{ checked }">
+              <li
+                class="mb-3 list-none rounded-lg border-2 border-neutral-200 p-3 font-semibold text-neutral-600"
+                :class="{
+                  ' border-neutral-400  bg-neutral-100 text-neutral-900': checked,
+                  ' bg-none': !checked,
+                }"
+              >
+                <div class="flex justify-between">
+                  <span>Online Payment</span>
+                  <Check v-show="checked" class="fill-green-600 stroke-green-50" />
+                </div>
+                <div
+                  v-if="selected === 'Online'"
+                  class="mt-3 border-t-2 border-neutral-400 bg-neutral-100 py-3"
+                >
+                  <form action="" class="w-full">
+                    <div class="mb-3">
+                      <label class="font-semibold" for="kaartnr">Cart Number</label>
+                      <input type="text" id="kaartnr" class="w-full px-1" />
+                    </div>
+                    <div class="flex w-full flex-col justify-between gap-2">
+                      <div class="flex flex-col gap-3 lg:flex-row">
+                        <label class="font-semibold" for="vdatum">Expire Date</label>
+                        <input class="px-1" type="month" id="vdatum" placeholder="02-2203" />
+                      </div>
+                      <div class="flex flex-col gap-3 lg:flex-row">
+                        <label class="font-semibold" for="CC">CVC / CVV</label>
+                        <input
+                          class="px-1"
+                          type="text"
+                          maxlength="4"
+                          placeholder="ex. 0157"
+                          id="CC"
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div></li
+            ></RadioGroupOption>
+
+            <RadioGroupOption value="Mobile" v-slot="{ checked }">
+              <li
+                class="mb-3 list-none rounded-lg border-2 border-neutral-200 p-3 font-semibold text-neutral-600"
+                :class="{
+                  ' border-neutral-400  bg-neutral-100 text-neutral-900': checked,
+                  ' bg-none': !checked,
+                }"
+              >
+                <div class="flex justify-between">
+                  <span>Mobile Payment</span>
+                  <Check v-show="checked" class="fill-green-600 stroke-green-50" />
+                </div>
+                <div
+                  v-if="selected === 'Mobile'"
+                  class="mt-3 border-t-2 border-neutral-400 bg-neutral-100 py-3"
+                >
+                  <QrcodeVue
+                    value="https://www.youtube.com/watch?v=lpvT-Fciu-4"
+                    :size="150"
+                    level="H"
+                    class="border-4 border-neutral-50"
+                  ></QrcodeVue>
+                </div>
+              </li>
+            </RadioGroupOption>
+          </RadioGroup>
         </div>
       </div>
-    </form>
-
+    </div>
     <!--Payment button-->
-    <div class="bottom-0 col-span-1 flex items-end justify-center">
+    <div class="flex justify-center">
       <button class="w-[65%] rounded-lg bg-red-700 px-6 py-2 font-bold text-neutral-50">Pay</button>
     </div>
   </div>
@@ -84,13 +172,27 @@ import { useRoute } from 'vue-router'
 import useCart from '../../composables/useCart'
 import PaymentForm from './components/PaymentForm.vue'
 import useUser from '../../composables/useUser'
+import { X as Clear, ChevronDown as Chevron, CheckCircle2 as Check } from 'lucide-vue-next'
+import { RadioGroup, RadioGroupOption, RadioGroupLabel } from '@headlessui/vue'
+import { vAutoAnimate } from '@formkit/auto-animate'
+import QrcodeVue from 'qrcode.vue'
+
 export default {
   components: {
     PaymentForm,
+    RadioGroup,
+    RadioGroupOption,
+    RadioGroupLabel,
+    Clear,
+    Chevron,
+    Check,
+    QrcodeVue,
   },
 
   setup() {
     const route = useRoute()
+    const selected = ref('Online')
+    const showFormM = ref(false)
     const { user } = useUser()
     let times: string[] = []
     let time = new Date()
@@ -98,6 +200,18 @@ export default {
     let m: Ref<string> = ref('')
     const deliveryType = computed(() => route.query.type)
     console.log(new Date().getHours())
+    const selectedTime = ref(times[0])
+    const city = ref('')
+    enum postalCodes {
+      Heule = 8501,
+      Kortrijk = 8500,
+      Bissegem = 8501,
+      Marke = 8510,
+      Bellegem = 8510,
+      Kooigem = 8510,
+      Rollegem = 8510,
+      Aalbeke = 8511,
+    }
 
     watch(m, () => {
       const newMethode = m
@@ -107,6 +221,24 @@ export default {
     watch(time, () => {
       makeTimes()
     })
+
+    watch(city, () => {
+      console.log(city.value)
+      if (postalCodes[city.value as keyof typeof postalCodes]) {
+        console.log('true')
+        city.value = city.value + ' ' + postalCodes[city.value as keyof typeof postalCodes]
+      }
+      if (user.value?.city) {
+        city.value = user.value?.city
+      } else {
+        city.value = city.value
+      }
+    })
+
+    //list of postal codes
+    const emptyInput = () => {
+      city.value = ''
+    }
     //function that makes a list of times in hour and minutes with a 15 minute interval and starting with 10:00 and ending with 23:00 and checkes if current time is in the list and if it is it will remove all times before the current time
     const makeTimes = () => {
       let hour = time.getHours()
@@ -115,7 +247,7 @@ export default {
 
       let timeNumber = Number(timeString.replace(':', '.'))
       let timeList = []
-      for (let i = 10; i < 24; i++) {
+      for (let i = 10; i < 23; i++) {
         for (let j = 0; j < 60; j += 15) {
           if (j == 0) {
             timeList.push(i + ':00')
@@ -144,7 +276,13 @@ export default {
       m,
       makeTimes,
       times,
+      postalCodes,
       deliveryType,
+      selectedTime,
+      city,
+      emptyInput,
+      selected,
+      showFormM,
     }
   },
 }
