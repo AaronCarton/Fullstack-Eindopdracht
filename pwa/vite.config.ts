6474bb75ec752ae33808b5a07e2384ae3be092a1
DefoortMerijn <merijn.defoort@student.howest.be>
2022-12-18 20:12:16 +0100

    feat: manifest

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import { VitePluginFonts } from 'vite-plugin-fonts'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
         ['font-title', { 'font-family': 'barber-complete, sans-serif;' }],
         ['font-subtitle', { 'font-family': 'fredoka one, sans-serif;' }],
       ],
+    }),
+    VitePWA({
+      strategies: 'generateSW',
     }),
     VitePluginFonts({
       typekit: {
